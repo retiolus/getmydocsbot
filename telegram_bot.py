97666e831dde2b6c1b86c66ec03e7607d4428276
retiolus <retiolus@protonmail.com>
2020-10-13 21:17:52 +0200

    API change

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,72 +1,82 @@
-import telebot
-from telebot import types
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 import os
 import os.path
 from os import path
 from pathlib import Path
+import time
+import logging
 
 TOKEN = '<TOKEN>'
 
 p_path = "</parent/path>" #parent directory
 b_path = "</bot/path>" #directory path of the bot
-gifs = "gifs/" #gifs directory path
 
-tb = telebot.TeleBot(TOKEN)	#create a new Telegram Bot object
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
-@tb.message_handler(commands=['start'])
-def start(message):
-    tb.reply_to(message, "<Your welcome message.>")
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="<Your welcome message.>")
 
-#send a tree structure of parent directory when user send /find
-@tb.message_handler(commands=['find'])
-def find(message):
-   os.system ("tree " + p_path + " > " + b_path + "/tree.txt") #create txt file with tree structure
-   doc = open(b_path + '/tree.txt')
-   tb.send_document(message.chat.id, doc)
+# def error(update, context):
+#     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-#send document
-@tb.message_handler(commands=['doc'])
-def doc(message):
-    c_message = message.text #clean message
-    l_c_message = len(c_message) #lenght of c_message
-    c_message = c_message[5:l_c_message] #delete "/doc" from c_message
+def find(update, context):
+    os.system ("tree " + p_path + " > " + b_path + "/tree.txt") #create txt file with tree structure
+    doc = open(b_path + '/tree.txt', 'rb')
+    chat_id = str(update.effective_chat.id)
+    context.bot.send_document(chat_id, document=doc)
 
-    f_path = os.popen("find " + p_path + " -type f -name '*" + c_message + "*'").read() #full path
-    l_f_path = len(f_path) #lenght of full path
-    f_path = f_path[0 : l_f_path-1]
-    p_list = f_path.split("\n") #create a list of paths
+def doc(update, context):
 
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True) #create the image selection keyboard
+    if not context.args:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="And the document?")
 
-    if len(p_list)==0: #if no path
-#        gif_p = os.popen("find gifs/ -type f | shuf -n 1").read()
-#        l_gif_p = len(gif_p)
-#        gif_p = gif_p[0 : l_gif_p-1]
-#        gif = open(gif_p, 'rb')
-#        tb.send_document(message.chat.id, gif, timeout=1000)
-        tb.reply_to(message, "This document does not exist or its name contains invalid characters.")
+    else:
+        c_message = str(context.args[0])
+        print(c_message)
 
-    elif len(p_list)==1: #if one path
-        my_file = Path(f_path)
-        if my_file.is_file(): #if path exists
-            doc = open(f_path, 'rb')
-            tb.send_document(message.chat.id, doc, timeout=1000)
-#            tb.delete_message(message.chat.id, message.message_id,timeout=1000)
-        else:
-#            gif_p = os.popen("find gifs/ -type f | shuf -n 1").read()
-#            l_gif_p = len(gif_p)
-#            gif_p = gif_p[0 : l_gif_p-1]
-#            gif = open(gif_p, 'rb')
-#            tb.send_document(message.chat.id, gif, timeout=1000)
-            tb.reply_to(message, "This document does not exist or its name contains invalid characters.")
+        f_path = os.popen("find " + p_path + " -type f -name '*" + c_message + "*'").read() #full path
+        print(f_path)
+        l_f_path = len(f_path) #lenght of full path
+        f_path = f_path[0 : l_f_path-1]
+        p_list = f_path.split("\n") #create a list of paths
 
-    else: #if more than one path
-#        tb.delete_message(message.chat.id, message.message_id,timeout=1000)
-        for i in p_list: #create the image selection keyboard
-            ic = os.path.split(i)
-            itembtn = types.KeyboardButton("/doc " + ic[1])
-            markup.row(itembtn)
+        if len(p_list)==0: #if no path
+            context.bot.send_message(chat_id=update.effective_chat.id, text="This document does not exist or its name contains invalid characters.")
 
-        bot_msg = tb.send_message(message.chat.id, "Document to be sent :", reply_markup=markup)
+        elif len(p_list)==1: #if one path
+            my_file = Path(f_path)
+            if my_file.is_file(): #if path exists
+                doc = open(f_path, 'rb')
+                chat_id = str(update.effective_chat.id)
+                context.bot.send_document(chat_id, document=doc)
 
-tb.polling(none_stop=True)
+            else:
+                context.bot.send_message(chat_id=update.effective_chat.id, text="This document does not exist or its name contains invalid characters.")
+
+        else: #if more than one path
+            keyboard=[]
+            for i in p_list:
+                ic = os.path.split(i)
+                itembtn = KeyboardButton("/doc " + ic[1])
+                keyboard.append([itembtn])
+
+            print(keyboard)
+
+            reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+            update.message.reply_text("Choisissez votre document: ", reply_markup=reply_markup)
+
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('find', find))
+    updater.dispatcher.add_handler(CommandHandler('doc', doc, pass_args=True))
+
+    updater.start_polling()
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
