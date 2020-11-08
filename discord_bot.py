cb73969c9597787e2855145080495c5d101c6a49
retiolus <retiolus@protonmail.com>
2020-11-08 04:33:43 +0100

    fixed security issue

--- discord_bot.py
+++ discord_bot.py
@@ -1,13 +1,14 @@
 import discord
-import config
 import os
 import os.path
 from os import path
 from pathlib import Path
+import subprocess
+import config
 
 TOKEN = config.DTOKEN
 
-p_path = config.FPATH #files directory
+p_path = config.FPATH #parent directory
 b_path = config.BPATH #directory path of the bot
 
 client = discord.Client()
@@ -26,38 +27,39 @@ async def on_message(message):
         doc = open(b_path + '/tree.txt')
         channel = client.get_channel(762760304186359892)
         await message.channel.send(file=discord.File(doc))
-        await message.delete(delay=5)
 
     if message.content.startswith('/doc'):
         c_message = message.content #clean message
         l_c_message = len(c_message) #lenght of c_message
         c_message = c_message[5:l_c_message] #delete "/doc" from c_message
 
-        f_path = os.popen("find " + p_path + " -type f -name '*" + c_message + "*'").read() #full path
-        l_f_path = len(f_path) #lenght of full path
-        f_path = f_path[0 : l_f_path-1]
-        p_list = f_path.split("\n") #create a list of paths
+        f_path = subprocess.check_output("find " + p_path + " -type f -name '*" + c_message + "*'", shell=True).splitlines()
+        print(f_path)
+        p_list = f_path
 
         if len(p_list)==0: #if no path
+            await message.delete(delay=30)
             await message.channel.send("This document does not exist or its name contains invalid characters.", delete_after=30)
 
         elif len(p_list)==1: #if one path
+            f_path = f_path[0].decode('utf-8')
             my_file = Path(f_path)
+            print(my_file)
             if my_file.is_file(): #if file exists
                 doc = open(f_path, 'rb')
                 file_name = os.path.split(f_path)
                 await message.channel.send(file=discord.File(doc, filename=str(file_name[1])))
 
             else:
-                await message.channel.send("This document does not exist or its name contains invalid characters.", delete_after=30)
+                await message.delete(delay=30)
+                await message.channel.send("This document does not exist, its name contains invalid characters or two documents have the same name.", delete_after=30)
 
         else: #if more than one file
-            await message.channel.send("Choose one of these documents. :arrow_heading_down:", delete_after=30)
+            await message.delete(delay=60)
+            await message.channel.send("Choose one of these documents. :arrow_heading_down:", delete_after=60)
             for i in p_list:
                 ic = os.path.split(i)
-                await message.channel.send(ic[1], delete_after=30)
-            await message.channel.send("Choose one of these documents. :arrow_heading_up:", delete_after=30)
-
-        await message.delete(delay=15)
+                await message.channel.send(ic[1].decode('utf-8'), delete_after=60)
+            await message.channel.send("Choose one of these documents. :arrow_heading_up:", delete_after=60)
 
 client.run(TOKEN)
