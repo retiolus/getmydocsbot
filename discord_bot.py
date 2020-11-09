0226c74153aea10b39f8957ecf0de36dd97b9331
retiolus <retiolus@protonmail.com>
2020-11-09 01:04:24 +0100

    added typing when bot sends document

--- discord_bot.py
+++ discord_bot.py
@@ -24,42 +24,44 @@ async def on_ready():
 
 @bot.command()
 async def find(ctx):
-    os.system ("tree " + p_path + " > " + b_path + "/tree.txt") #create txt file with tree structure
-    doc = open(b_path + '/tree.txt')
-    # channel = client.get_channel(762760304186359892)
-    await ctx.message.channel.send(file=discord.File(doc))
+    async with ctx.typing():
+        os.system ("tree " + p_path + " > " + b_path + "/tree.txt") #create txt file with tree structure
+        doc = open(b_path + '/tree.txt')
+        # channel = client.get_channel(762760304186359892)
+        await ctx.message.channel.send(file=discord.File(doc))
 
 @bot.command()
 async def doc(ctx, arg):
-    c_message = arg
+    async with ctx.typing():
+        c_message = arg
 
-    f_path = subprocess.check_output("find " + p_path + " -type f -name '*" + c_message + "*'", shell=True).splitlines()
-    print(f_path)
-    p_list = f_path
+        f_path = subprocess.check_output("find " + p_path + " -type f -name '*" + c_message + "*'", shell=True).splitlines()
+        print(f_path)
+        p_list = f_path
 
-    if len(p_list)==0: #if no path
-        await ctx.message.delete(delay=30)
-        await ctx.message.channel.send("This document does not exist or its name contains invalid characters.", delete_after=30)
-
-    elif len(p_list)==1: #if one path
-        f_path = f_path[0].decode('utf-8')
-        my_file = Path(f_path)
-        print(my_file)
-        if my_file.is_file(): #if file exists
-            doc = open(f_path, 'rb')
-            file_name = os.path.split(f_path)
-            await ctx.message.channel.send(file=discord.File(doc, filename=str(file_name[1])))
-
-        else:
+        if len(p_list)==0: #if no path
             await ctx.message.delete(delay=30)
-            await ctx.message.channel.send("This document does not exist, its name contains invalid characters or two documents have the same name.", delete_after=30)
+            await ctx.message.channel.send("This document does not exist or its name contains invalid characters.", delete_after=30)
 
-    else: #if more than one file
-        await ctx.message.delete(delay=60)
-        await ctx.message.channel.send("Choose one of these documents. :arrow_heading_down:", delete_after=60)
-        for i in p_list:
-            ic = os.path.split(i)
-            await ctx.message.channel.send(ic[1].decode('utf-8'), delete_after=60)
-        await ctx.message.channel.send("Choose one of these documents. :arrow_heading_up:", delete_after=60)
+        elif len(p_list)==1: #if one path
+            f_path = f_path[0].decode('utf-8')
+            my_file = Path(f_path)
+            print(my_file)
+            if my_file.is_file(): #if file exists
+                doc = open(f_path, 'rb')
+                file_name = os.path.split(f_path)
+                await ctx.message.channel.send(file=discord.File(doc, filename=str(file_name[1])))
+
+            else:
+                await ctx.message.delete(delay=30)
+                await ctx.message.channel.send("This document does not exist, its name contains invalid characters or two documents have the same name.", delete_after=30)
+
+        else: #if more than one file
+            await ctx.message.delete(delay=60)
+            await ctx.message.channel.send("Choose one of these documents. :arrow_heading_down:", delete_after=60)
+            for i in p_list:
+                ic = os.path.split(i)
+                await ctx.message.channel.send(ic[1].decode('utf-8'), delete_after=60)
+            await ctx.message.channel.send("Choose one of these documents. :arrow_heading_up:", delete_after=60)
 
 bot.run(TOKEN)
